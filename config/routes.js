5a9ebcc0002c502d06ffbcf8c52d234225348b5c
irfanp90 <irfanp90@gmail.com>
2019-04-22 20:35:24 -0500

    update

--- config/routes.js
+++ config/routes.js
@@ -15,9 +15,9 @@ module.exports = function(app){
     app.get("/saved", function(req,res){
         res.render("saved");
     });
-    app. get("/api/fetch", function (req,res){
-            headlinesController.fetch(function(err,docs){
-            if (!docs || docs.insertedCount === 0){
+app.get("/api/fetch", function (req,res){
+        headlinesController.fetch(function(err,docs){
+         if (!docs || docs.insertedCount === 0){
                 res.json({
                     message: "No new article"
                 })
@@ -27,16 +27,49 @@ module.exports = function(app){
                     message: "Added" + docs.insertedCount + "new article"
                 })
             } 
-            });
-    });
-    app. get("/api/headlines", function (req,res){
+     });
+});
+ app.get("/api/headlines", function (req,res){
         var query = {};
         if (req.query.saved){
-            query = req. query;
+            query = req.query;
         }
     headlinesController.get(query, function (data){
         res.json(data);
     });
 });
-
+app.delete("/api/headlines/:id", function (req,res){
+    var query = {};
+    query._id = req.params.id;
+    headlinesController.delete(query, function(err,data){
+        res.json(data);
+    });
+});
+app.post("/api/headlines", function (req,res){
+    
+    headlinesController.update(req.body, function(err,data){
+        res.json(data);
+    });
+});
+app.get("/api/notes/:headline_id?", function (req,res){
+    var query = {};
+  if(req.params.headline_id){
+   query._id =req.params.headline_id;
+}
+notesController.get(query, function(err,data){
+    res.json(data);
+    });
+});
+app.delete("/api/notes/:id", function (req,res){
+    var query = {};
+    query._id = req.params.id;
+    notesController.delete(query, function(err,data){
+        res.json(data);
+    });
+});
+app.post("/api/notes", function (req,res){
+    notesController.save(req.body, function(err,data){
+    res.json(data);
+    });
+});
 }
